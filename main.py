77d5fa86607a1da4cf351f4d5bf4840602167cd8
danameyer <dmeyer6@uni-bielefeld.de>
2024-06-11 16:58:50 +0200

    set up project structure. Refs #2.

---
+++ main.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+TrOCR inference service for the Flow project environment.
+
+:REQUIRES:
+transformers
+fastapi
+
+:TODO:
+everything :)
+
+:AUTHOR: Dana Meyer and Jonas Widmer
+:ORGANIZATION: Flow Project
+:CONTACT: jonas.widmer@unibe.ch
+:SINCE: %(date)s
+:VERSION: 0.1
+"""
+# ===============================================================================
+# PROGRAM METADATA
+# ===============================================================================
+__author__ = 'Dana Meyer and Jonas Widmer'
+__contact__ = 'jonas.widmer@unibe.ch'
+__copyright__ = 'Please cite the Flow project'
+__license__ = 'MIT'
+__date__ = '%(date)s'
+__version__ = '0.1'
+
+# ===============================================================================
+# IMPORT STATEMENTS
+# ===============================================================================
+from fastapi import FastAPI, Depends
+
+
+# ===============================================================================
+# METHODS
+# ===============================================================================
+
+
+# ===============================================================================
+# MAIN METHOD AND TESTING AREA
+# ===============================================================================
+def main():
+    """Description of main()"""
+
+
+if __name__ == '__main__':
+    main()
