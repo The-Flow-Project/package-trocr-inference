e0d1a9008657a21b46cf508a47a5d8f970122876
danameyer <dmeyer6@uni-bielefeld.de>
2024-12-17 18:39:03 +0100

    improve typing and comments for data handling. Refs #8.

--- flow_inference/data_handling.py
+++ flow_inference/data_handling.py
@@ -1,39 +1,50 @@
 import os
-from typing import List
-
+from typing import List, Tuple
 import dotenv
 from flow_githubmanager.github_interaction import GitHubManager
 
 
 class DataHandler:
-    def __init__(self, in_path: str, out_path: str):
+    """
+    Class for handling logic related to file transfers from and to GitHub repositories.
+    """
+
+    def __init__(self, download_path: str, upload_path: str):
+        """
+        :param download_path: the (local) path the files are taken from.
+        :param upload_path: the (local) path the files are taken from when uploaded.
+        """
         dotenv.load_dotenv()
         access_token = os.getenv("GITHUB_ACCESS_TOKEN")
         self.github_manager = GitHubManager(access_token)
-        self.in_path = in_path
-        self.out_path = out_path
+        self.in_path = download_path
+        self.out_path = upload_path
 
     def fetch_xml_files_from_github(self,
                                     repo_name: str,
-                                    file_output_path: str):
-        preprocessed_files = self.github_manager.fetch_files(repo_name,
-                                                             "xml",
-                                                             ".xml",
-                                                             file_output_path)
-        return preprocessed_files
+                                    download_path: str) -> Tuple[List[str], List[str]]:
+        """
+        Fetches XML files from GitHub.
+
+        :param repo_name: the name of the GitHub repository the files are fetched from.
+        :param download_path: the (local) path the files are downloaded to.
+        :return: Tuple consisting of list of files which were downloaded from the GitHub repository and
+        list of files which couldn't be downloaded.
+        """
+        fetched_files = self.github_manager.fetch_files(repo_name,
+                                                        "xml",
+                                                        ".xml",
+                                                        download_path)
+        return fetched_files
 
     def push_xml_files_to_github(self,
                                  repo_name: str,
-                                 file_paths: List[str]):
+                                 file_paths: List[str]) -> None:
+        """
+        Pushes XML to a GitHub repository.
+
+        :param repo_name: the name of the GitHub repository the files are pushed to.
+        :param file_paths: the files which are uploaded to the GitHub repository (with their paths specified).
+        :return: None.
+        """
         self.github_manager.upload_documents(repo_name, file_paths, folder_name="inference")
-
-
-if __name__ == '__main__':
-    current_dir: str = os.path.dirname(os.path.realpath(__file__))
-    in_path_github: str = os.path.join(current_dir, "..", "github_download")
-    if not os.path.exists(in_path_github):
-        os.makedirs(in_path_github)
-    data_handler = DataHandler(in_path_github, in_path_github)
-    data_handler.fetch_xml_files_from_github("github-actions-test-organisation/github-interaction-test",
-                                             in_path_github)
-
